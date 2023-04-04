59b33dc16f0141a165003452b51bcccc2902f173
Thainara da Silva <thainaracaracol@gmail.com>
2023-04-03 22:53:08 -0300

    subindo a minha lição do dia 03/04

---
+++ atividade0304/src/Header.jsx
@@ -0,0 +1,15 @@
+import './header.css'
+
+function Header() {
+    return (
+      <header className="Header">
+        <h1>Sou um cabeçalho</h1>
+        <p>
+            lorem ipsum dolor sit amet, consectetur adip occurence, sed do eiusmod tempor incididunt ut lab et dolore magna aliqua. Ut enim ad minim ven quis nostrud ex ea commodo consequat. lorem ipsum dolor sit amet, consectetur adip occurence, sed do eiusmod tempor incididunt ut lab et dolore magna aliqua. Ut enim ad minim ven quis nostrud ex ea commodo consequat. orem ipsum dolor sit amet, consectetur adip occurence, sed do eiusmod tempor incididunt ut lab et dolore magna aliqua. Ut enim ad minim ven quis nostrud ex ea commodo consequat. lorem ipsum dolor sit amet, consectetur adip occurence, sed do eiusmod tempor incididunt ut lab et dolore magna aliqua. Ut enim ad minim ven quis nostrud ex ea commodo consequat.
+        </p>
+      </header>
+    )
+  }
+  
+  export default Header
+  
